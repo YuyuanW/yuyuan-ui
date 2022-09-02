83f3c5c632a0e20bdd1a57078b6ca724118c6601
YuyuanW <1413733614@qq.com>
2022-09-02 16:12:59 +0800

    创建Dialog

---
+++ src/components/docComponent/DialogDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Dialog示例
+    </div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
+    
+</template>
+
+<script lang="ts">
+import Dialog from '../../lib/Dialog.vue';
+export default {
+    name: "DialogDemo",
+    components: { Dialog }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <Button>Cancel</Button>
+            <Button>OK</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+export default ({
+    name:'Dialog',
+    components:{Button},
+})
+</script>
