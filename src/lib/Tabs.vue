304b4b305c3e68289a9af77ef30da7f39d89fe64
YuyuanW <1413733614@qq.com>
2022-09-04 00:50:47 +0800

    Tabs html结构

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>Tabs</div>
+    
+    <div class="yuyuan-tabs">
+        <div class="yuyuan-tabs-nav">
+            <div class="yuyuan-tabs-nav-item" v-for="(t,index) in titles" :key="index" >{{t}}</div>
+        </div>
+        <div class="yuyuan-tabs-content">
+            <component class="yuyuan-tabs-content-item" v-for="(c,index) in defaults" :key="index" :is="c"></component>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -7,12 +15,16 @@ export default ({
     name:'Tabs',
     setup(props,context){
         const defaults = context.slots.default()
+        // console.log(defaults[0].props.title);
         defaults.forEach(element => {
             if(element.type.name !== 'Tab'){
                 throw new Error('Tabs的子标签必须为Tab')
             }
+        })
+        const titles = defaults.map(element => {
+            return element.props.title
         });
-        
+        return {defaults,titles}
     }
 })
 </script>
