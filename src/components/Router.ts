4df39e43685d6112ca1bbbf5f259c84987958990
YuyuanW <1413733614@qq.com>
2022-09-05 22:33:26 +0800

    事不过三  封装路由

--- src/components/Router.ts
+++ src/components/Router.ts
@@ -7,17 +7,17 @@ import ButtonDemo from '../components/docComponent/ButtonDemo.vue'
 import DialogDemo from '../components/docComponent/DialogDemo.vue'
 import TabsDemo from '../components/docComponent/TabsDemo.vue'
 import DocDemo from '../components/docComponent/DocDemo.vue'
-import Intro from '../views/Intro.vue'
-import Install from '../views/Install.vue'
-import GetStart from '../views/GetStart.vue'
+import Markdown from '../components/Markdown.vue'
+import { h } from 'vue'
+const md = (filename)=>h(Markdown,{path:'../markdown/'+filename+'.md',key:filename})
 
 const routes = [
     { path: '/', component: Home },
     { path: '/doc', component: Doc,
       children:[
-        {path:'intro',component:Intro},
-        {path:'install',component:Install},
-        {path:'get-start',component:GetStart},
+        {path:'intro',component:md('intro')},
+        {path:'install',component:md('install')},
+        {path:'get-start',component:md('get-start')},
         {path:'',component:DocDemo},
         {path:'switch',component:SwitchDemo},
         {path:'button',component:ButtonDemo},
