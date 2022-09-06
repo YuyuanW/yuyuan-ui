45c5e54c7709ac4252bbe3ca9f99380b8a6733ac
YuyuanW <1413733614@qq.com>
2022-09-07 00:43:50 +0800

    解决rollup无法满足动态加载的问题(build)可检验

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,22 +1,14 @@
 <template>
-    <article class="markdown-body" v-html="content"></article>
+  <article class="markdown-body" v-html="content"></article>
 </template>
 
 <script lang="ts">
-  import { ref } from "vue"
-export default ({
-    props:{
-      path:{
-        type:String,
-        require:true,
-      }
+export default {
+  props: {
+    content: {
+      type: String,
+      require: true,
     },
-    setup(props){
-      const content = ref< string >('')
-      import(props.path).then(result=>{
-        content.value = result.default
-      })
-      return {content}
-    },
-})
+  },
+};
 </script>
--- src/components/Router.ts
+++ src/components/Router.ts
@@ -7,17 +7,21 @@ import ButtonDemo from '../components/docComponent/ButtonDemo.vue'
 import DialogDemo from '../components/docComponent/DialogDemo.vue'
 import TabsDemo from '../components/docComponent/TabsDemo.vue'
 import Markdown from '../components/Markdown.vue'
+import intro from '../markdown/intro.md'
+import install from '../markdown/install.md'
+import getStart from '../markdown/get-start.md'
+
 import { h } from 'vue'
-const md = (filename)=>h(Markdown,{path:'../markdown/'+filename+'.md',key:filename})
+const md = (string)=>h(Markdown,{content:string,key:string})
 
 const routes = [
     { path: '/', component: Home },
     { path: '/doc', component: Doc,
       children:[
         {path:'',redirect:'/doc/intro'},
-        {path:'intro',component:md('intro')},
-        {path:'install',component:md('install')},
-        {path:'get-start',component:md('get-start')},
+        {path:'intro',component:md(intro)},
+        {path:'install',component:md(install)},
+        {path:'get-start',component:md(getStart)},
         {path:'switch',component:SwitchDemo},
         {path:'button',component:ButtonDemo},
         {path:'dialog',component:DialogDemo},
