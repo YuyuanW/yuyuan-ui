b0c75a992a7274fb72efbc9b682cd2ed2a36a4db
YuyuanW <1413733614@qq.com>
2022-09-06 23:55:21 +0800

    完成Dialog组件

---
+++ src/components/TabsDemo/Tabs1.vue
@@ -0,0 +1,30 @@
+<demo>
+    常规使用
+</demo>
+
+<template>
+  <div>
+    <Tabs v-model:selected="x">
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import Tabs from "../../lib/Tabs.vue";
+import Tab from "../../lib/Tab.vue";
+import { ref } from "vue";
+
+export default {
+  name: "Tabs1",
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup() {
+    const x = ref("导航1");
+    return { x, Tabs, Tab };
+  },
+};
+</script>
