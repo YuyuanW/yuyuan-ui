c8f72bd371d3b13127190b15981e3bd416d6b9c7
YuyuanW <1413733614@qq.com>
2022-09-07 00:00:47 +0800

    路由重定向，首页定向优化

--- src/components/Router.ts
+++ src/components/Router.ts
@@ -6,7 +6,6 @@ import SwitchDemo from '../components/docComponent/SwitchDemo.vue'
 import ButtonDemo from '../components/docComponent/ButtonDemo.vue'
 import DialogDemo from '../components/docComponent/DialogDemo.vue'
 import TabsDemo from '../components/docComponent/TabsDemo.vue'
-import DocDemo from '../components/docComponent/DocDemo.vue'
 import Markdown from '../components/Markdown.vue'
 import { h } from 'vue'
 const md = (filename)=>h(Markdown,{path:'../markdown/'+filename+'.md',key:filename})
@@ -15,10 +14,10 @@ const routes = [
     { path: '/', component: Home },
     { path: '/doc', component: Doc,
       children:[
+        {path:'',redirect:'/doc/intro'},
         {path:'intro',component:md('intro')},
         {path:'install',component:md('install')},
         {path:'get-start',component:md('get-start')},
-        {path:'',component:DocDemo},
         {path:'switch',component:SwitchDemo},
         {path:'button',component:ButtonDemo},
         {path:'dialog',component:DialogDemo},
