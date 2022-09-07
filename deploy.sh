a7574d6cb2a0ff321ac70815749a8c7de57f9037
YuyuanW <1413733614@qq.com>
2022-09-07 16:37:54 +0800

    成功连接

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-rm -rf dist
-yarn build
-cd dist
-git init
-git add .
-git commit -m 'update'
-git branch -M master
-git remote add origin git@github.com:YuyuanW/yuyuan-ui-use.git
-git push -f -u origin master
+rm -rf dist &&
+yarn build && 
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M master &&
+git remote add origin git@github.com:YuyuanW/yuyuan-ui-use.git && 
+git push -f -u origin master && 
 cd -
 echo https://yuyuanw.github.io/yuyuan-ui-use
